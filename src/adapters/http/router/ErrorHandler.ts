4f31afc8f2da8c509f30178368e590bd35749838
julien rollin <1086565+jrollin@users.noreply.github.com>
2020-07-03 17:55:18 +0200

    fix: json response on error handler

--- src/adapters/http/router/ErrorHandler.ts
+++ src/adapters/http/router/ErrorHandler.ts
@@ -1,16 +1,16 @@
-import {Application, Request, Response} from 'express';
+import {Application, NextFunction, Request, Response} from 'express';
 import {ValidationError} from '../../../core/domain/ValidationError';
 import {ProjectNotFound} from '../../../core/port/ProjectNotFound';
 
 export const configureErrorHandler = (
     app: Application,
 ) => {
-    app.use((err: any, req: Request, res: Response) => {
+    app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
         if (err instanceof ValidationError) {
-            return res.status(422).send(err.getErrors())
+            return res.status(422).json(err.getErrors())
         }
         if (err instanceof ProjectNotFound) {
-            return res.status(404).send('Project not found')
+            return res.status(404).json('Project not found')
         }
         const message: string = 'Something went wrong'
         res.status(500).json(message)
